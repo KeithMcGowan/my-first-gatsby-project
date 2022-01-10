432494e8b422d96149dcf1243d1fce76105fe4aa
Keith <keithrichardmcgowan@gmail.com>
2022-01-10 17:46:41 -0500

    Finished Gatsby Tutorial Part 7.

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,14 +2,28 @@ import React from "react";
 import Layout from "../../components/layout";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const BlogPost = ({ data }) => {
   const { frontmatter, body } = data.mdx;
-  const { title, date } = frontmatter;
+  const {
+    title,
+    date,
+    hero_image,
+    hero_image_alt,
+    hero_image_credit,
+    hero_image_credit_link,
+  } = frontmatter;
+  const image = getImage(hero_image);
 
   return (
     <Layout pageTitle={title}>
-      <p>{date}</p>
+      <p>Posted: {date}</p>
+      <GatsbyImage image={image} alt={hero_image_alt} />
+      <p>
+        Photo Credit: {" "}
+        <a href={hero_image_credit_link}>{hero_image_credit}</a>
+      </p>
       <MDXRenderer>{body}</MDXRenderer>
     </Layout>
   );
@@ -21,6 +35,14 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        hero_image_alt
+        hero_image_credit
+        hero_image_credit_link
       }
       body
     }
